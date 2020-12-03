964f67290dd050095874b78cc217c86b91cb4d4a
phanikota9 <74732607+phanikota9@users.noreply.github.com>
2020-12-02 21:00:18 -0500

    Update azure.tf

--- azure/azure.tf
+++ azure/azure.tf
@@ -52,7 +52,7 @@ resource "azurerm_network_security_group" "main" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "22"
-        source_address_prefix      = module.whitelist.vpn_ops_ipv4
+        source_address_prefixes    = module.whitelist.vpn_ops_ipv4
         destination_address_prefix = "*"
     }
 
